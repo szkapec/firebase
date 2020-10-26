b3ece55c2553333f7f51eef47d58958bf281b26a
szkapec <szkapec999@o2.pl>
2020-10-26 19:44:24 +0100

    Updates

---
+++ precache-manifest.5787680a1871a28f81522eb8287f64ad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071ab9c993f5656e63d39c68ed6cf2fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0fd9e073a2ac9d453f7",
+    "url": "/static/css/main.12ccff71.chunk.css"
+  },
+  {
+    "revision": "2fcdf968b0639d579466",
+    "url": "/static/js/2.e9c87f95.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.e9c87f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0fd9e073a2ac9d453f7",
+    "url": "/static/js/main.f1b2d610.chunk.js"
+  },
+  {
+    "revision": "f575ecee599cfbcbd7c2",
+    "url": "/static/js/runtime-main.68d1b4b8.js"
+  },
+  {
+    "revision": "a28769c8810374a770d52c723851367d",
+    "url": "/static/media/facebook.a28769c8.PNG"
+  },
+  {
+    "revision": "bc95bc70706ee9ef3f66ee1f06d8fa55",
+    "url": "/static/media/instagram.bc95bc70.PNG"
+  },
+  {
+    "revision": "6432577414f0fe64044107797cee9459",
+    "url": "/static/media/linkedin.64325774.PNG"
+  },
+  {
+    "revision": "1fef8ef319f4e8c65a8e6f535eb2965f",
+    "url": "/static/media/logo_transparent.1fef8ef3.png"
+  },
+  {
+    "revision": "6849f9d46b0818c1baea625269c060a3",
+    "url": "/static/media/twitter.6849f9d4.PNG"
+  },
+  {
+    "revision": "c0f30fc56231fb4e1ddfeaa4d7364aad",
+    "url": "/static/media/youtube.c0f30fc5.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/firebase/precache-manifest.e1b07499667a4e6578859cc4e0a54149.js"
+  "/precache-manifest.5787680a1871a28f81522eb8287f64ad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/firebase/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
